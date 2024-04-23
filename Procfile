6293b3ed08c61b79fcec4d39008aabe39593e07d
Nick Ward <nick@klarventures.onmicrosoft.com>
2024-04-23 08:07:54 -0400

    Heroku deployment

--- Procfile
+++ Procfile
@@ -1,3 +1,12 @@
-web: waitress-serve --port=$PORT donation_map.wsgi:application
+#web: waitress-serve --port=$PORT donation_map.wsgi:application
 #web: gunicorn donations_map:app
 #web: waitress-serve --port=127.0.0.8050 donations_map:app
+
+web: waitress-serve \
+    --listen "*:$PORT" \
+    --trusted-proxy '*' \
+    --trusted-proxy-headers 'x-forwarded-for x-forwarded-proto x-forwarded-port' \
+    --log-untrusted-proxy-headers \
+    --clear-untrusted-proxy-headers \
+    --threads ${WEB_CONCURRENCY:-4} \
+    donations_map:wsgifunc
--- donations_map.py
+++ donations_map.py
@@ -13,77 +13,10 @@ Compress(server)
 server.config['COMPRESS_MIN_SIZE'] = 500  # Compress responses over 500 bytes
 app = dash.Dash(__name__, server=server)
 
+wsgifunc = server
+
 with open(r'C:\Users\NickWard\OneDrive\Apps\Donations map vDEPLOY\Mapping\gz_2010_us_050_00_20m.json', 'r') as f:
     counties = json.load(f)
-
-#This is code for lower dimensionality GeoJSON
-'''for feature in counties['features']:
-    # Ensure FIPS is five digits long
-    county_code = feature['id']
-    fips_code = county_code.zfill(5)  # Add leading zero if needed
-    feature['properties']['FIPS'] = fips_code'''
-
-# Sample CSV file that maps FIPS codes to state information
-'''state_fips_mapping = {
-    "01": "Alabama",
-    "02": "Alaska",
-    "04": "Arizona",
-    "05": "Arkansas",
-    "06": "California",
-    "08": "Colorado",
-    "09": "Connecticut",
-    "10": "Delaware",
-    "11": "District of Columbia",
-    "12": "Florida",
-    "13": "Georgia",
-    "15": "Hawaii",
-    "16": "Idaho",
-    "17": "Illinois",
-    "18": "Indiana",
-    "19": "Iowa",
-    "20": "Kansas",
-    "21": "Kentucky",
-    "22": "Louisiana",
-    "23": "Maine",
-    "24": "Maryland",
-    "25": "Massachusetts",
-    "26": "Michigan",
-    "27": "Minnesota",
-    "28": "Mississippi",
-    "29": "Missouri",
-    "30": "Montana",
-    "31": "Nebraska",
-    "32": "Nevada",
-    "33": "New Hampshire",
-    "34": "New Jersey",
-    "35": "New Mexico",
-    "36": "New York",
-    "37": "North Carolina",
-    "38": "North Dakota",
-    "39": "Ohio",
-    "40": "Oklahoma",
-    "41": "Oregon",
-    "42": "Pennsylvania",
-    "44": "Rhode Island",
-    "45": "South Carolina",
-    "46": "South Dakota",
-    "47": "Tennessee",
-    "48": "Texas",
-    "49": "Utah",
-    "50": "Vermont",
-    "51": "Virginia",
-    "53": "Washington",
-    "54": "West Virginia",
-    "55": "Wisconsin",
-    "56": "Wyoming"
-}'''
-
-# Map state information to the GeoJSON
-'''for feature in counties['features']:
-    # Extract state code from FIPS
-    fips_code = feature['properties']['FIPS']
-    state_code = fips_code[:2]  # First two digits are the state code
-    feature['properties']['STATE'] = state_fips_mapping.get(state_code, "Unknown")  # Add state info'''
 
 #This is the code for the high dimension GeoJSON
 for feature in counties['features']:
